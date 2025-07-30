5e33539c541aab6500a955aa531727f67192ea5d
ianmuqrish <muqrishian@gmail.com>
2025-07-30 19:22:53 +0800

    Add files via upload

---
+++ menu items.sql
@@ -0,0 +1,44 @@
+-- 1. View the menu_items table.
+select *
+from menu_items;
+
+-- 2. Find the number of items on the menu.
+select count(*)
+from menu_items
+
+-- 3. What are the least and most expensive items on the menu?
+select *
+from menu_items
+order by price;  -- least expensive
+
+select *
+from menu_items
+order by price desc;  -- most expensive
+
+
+-- 4. How many Italian dishes are on the menu?
+select count(*) as italian_dishes
+from menu_items
+where category = 'Italian'
+
+-- 5. What are the least and most expensive Italian dishes on the menu?
+select *
+from menu_items
+where category = 'Italian'
+order by price  -- least expensive
+
+select *
+from menu_items
+where category = 'Italian'
+order by price desc;  -- most expensive 
+
+-- 6. How many dishes are there in each category?
+select category, count(menu_item_id) as num_dishes
+from menu_items
+group by category;
+
+-- 7. What is the average dish price wihthin each category?
+select category, avg(price) as avg_price
+from menu_items
+group by category;
+
